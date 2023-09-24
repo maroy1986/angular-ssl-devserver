493e245226ee0a17bdeb5f359b0137caa270c875
Marc-Andre Roy <me@marcandreroy.ca>
2023-09-23 20:14:21 -0400

    Add test-edge for unit testing on edge

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-ssl-devserver'`, () => {
+  it(`should have as title 'Angular dev with local SSL`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-ssl-devserver');
+    expect(app.title).toEqual('Angular dev with local SSL');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-ssl-devserver!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular dev with local SSL');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ssl-devserver';
+  title = 'Angular dev with local SSL';
 }
