6e75cb516d3e70230e91c07481551e7fd589afcb
Marc-Andre Roy <me@marcandreroy.ca>
2023-11-27 20:01:05 -0500

    Fix tests following Angular 17 upgrade

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [AppComponent],
+            declarations: [AppComponent ]
         }).compileComponents();
     });
 
@@ -14,16 +15,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it('should have the \'angular17\' title', () => {
+    it('should have the \'Angular dev with local SSL \' title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.componentInstance;
-        expect(app.title).toEqual('angular17');
+        expect(app.title).toEqual('Angular dev with local SSL');
     });
 
     it('should render title', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular17');
+        expect(compiled.querySelector('h1')?.textContent).toContain('Angular dev with local SSL');
     });
 });
